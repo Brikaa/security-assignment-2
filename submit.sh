ada40949bdc97d5ce23432d0e6571304550c3692
Omar Brikaa <brikaaomar@gmail.com>
2024-04-30 22:36:55 +0300

    Submission script

--- submit.sh
+++ submit.sh
@@ -1,3 +1,7 @@
+if [[ `git status --porcelain` ]]; then
+    echo "Uncommitted changes, aborting" && exit 1
+fi
+git clean -xfd
 rm -rf ./submission && echo "Removed submission directory (if any)" || exit 1
 mkdir -p ./submission/S5_Omar-Adel-Brikaa_20206043 && echo "Created a submission directory" || exit 1
 cd report && echo "Preparing to compile the PDF" || exit 1
